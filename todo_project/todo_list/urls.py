61d6c01dcd0dbd8d693bd1333096c369f41e388a
rostislav_filipenko <rastislav.filipenko@yandex.ru>
2025-03-28 17:43:09 +0300

    create urlpatterns

--- todo_project/todo_list/urls.py
+++ todo_project/todo_list/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from . import views
 
 app_name = "todo_list"
 
 urlpatterns = [
-    path("",
-         TemplateView.as_view(
-             template_name="todo_list/index.html"
-         ),
-         name="index")
+    # path("", views.index_view, name="index")
+    path("", views.ToDoListIndexView.as_view(), name="index"),
+    path("<int:pk>/", views.ToDoDetailView.as_view(), name="detail"),
+    path("list/", views.ToDoListView.as_view(), name="list"),
+    path("done/", views.ToDoListDoneView.as_view(), name="done"),
 ]
