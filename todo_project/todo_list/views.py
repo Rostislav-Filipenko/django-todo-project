0c3e6a19f41519c11a099bb68400e0550542cf79
rostislav_filipenko <rastislav.filipenko@yandex.ru>
2025-03-28 17:43:39 +0300

    add view

--- todo_project/todo_list/views.py
+++ todo_project/todo_list/views.py
@@ -1,3 +1,31 @@
+from django.http import (
+    HttpResponse, HttpRequest
+)
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView
 
-# Create your views here.
+from .models import ToDoItem
+
+def index_view(request: HttpRequest) -> HttpResponse:
+    todo_items = ToDoItem.objects.all()[:3]
+    return render(
+        request,
+        template_name="todo_list/index.html",
+        context={"todo_items": todo_items}
+    )
+
+
+class ToDoListIndexView(ListView):
+    template_name = "todo_list/index.html"
+    queryset = ToDoItem.objects.all()[:2]
+
+class ToDoListView(ListView):
+    model = ToDoItem
+
+class ToDoListDoneView(ListView):
+    queryset = ToDoItem.objects.filter(done=True).all()[:2]
+
+
+class ToDoDetailView(DetailView):
+    model = ToDoItem
+
