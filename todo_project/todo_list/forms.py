e470023e1438971a171d6420841e6fe2730ccbbe
rostislav_filipenko <rastislav.filipenko@yandex.ru>
2025-03-31 13:30:20 +0300

    add forms.py

---
+++ todo_project/todo_list/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+from .models import ToDoItem
+
+
+class ToDoItemForm(forms.ModelForm):
+
+    class Meta:
+        model = ToDoItem
+        fields = ('title', "description")
+
+        widgets = {
+            "description": forms.Textarea(attrs={"cols": 30, "rows": 5}),
+        }
