e03402c6616a6e711bc2674175cab29d0c561996
rostislav_filipenko <rastislav.filipenko@yandex.ru>
2025-03-31 13:31:24 +0300

    add class method get_absolute_url

--- todo_project/todo_list/models.py
+++ todo_project/todo_list/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class ToDoItem(models.Model):
@@ -8,7 +9,11 @@ class ToDoItem(models.Model):
         verbose_name  = 'ToDo Item'
 
     title = models.CharField(max_length=250)
+    description = models.TextField(blank=True, null=False)
     done = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse("todo_list:detail", kwargs={"pk": self.pk},)
 
     def __str__(self):
         return self.title
